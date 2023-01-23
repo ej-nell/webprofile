87fd2cddd59e12e7bdb666e7e23b82a16ef48880
ej-nell <ejnurbina@gmail.com>
2023-01-23 17:08:09 +0800

    Add responsiveness for different devices

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+const hamburger = document.querySelector("#hamburger");
+
+const navBar = document.querySelector("#nav-header");
+hamburger.onclick = function () {
+  hamburger.classList.toggle("active");
+  navBar.classList.toggle("active");
+};
+
+document.onclick = function (clickEvent) {
+  if (
+    clickEvent.target.id !== "navBar" &&
+    clickEvent.target.id !== "hamburger"
+  ) {
+    hamburger.classList.remove("active");
+    navBar.classList.remove("active");
+  }
+};
